d467c42af4913347875d5caabf6c39e746d8e809
Mattéo Grellier <matteogrellier@gmail.com>
2024-04-19 12:35:11 +0200

    Update lab

--- src/main/java/labJunit/app/Main.java
+++ src/main/java/labJunit/app/Main.java
@@ -4,12 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		Calculatrice c = new Calculatrice();
-		
+
 		double s = c.somme(10, 20);
-		System.out.println("La somme de 10 et 20 : "+s);
-		
+		System.out.println("La somme de 10 et 20 : " + s);
+
 		double p = c.produit(10, 20);
-		System.out.println("Le produit de 10 et 20 : "+p);
+		System.out.println("Le produit de 10 et 20 : " + p);
+
+		CompteBancaire cb = new CompteBancaire();
+
+		double cr = cb.crediterSolde(2);
+		System.out.println("Le solde après crédit est de : " + cr);
+
+		double d = cb.debiterSolde(5);
+		System.out.println("Le solde après débit est de : " + d);
 
 	}
 
